a07464a4ae26eb502bd0100d15028501c264fb1b
Akobir <akobirortiqov40@gmail.com>
2025-05-01 20:00:59 +0500

    create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def Auth_menu():
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    else:
+        print("Invalid choice!")
+        return Auth_menu()
