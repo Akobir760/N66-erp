627b45000811f7df2694789672f45f66b17d1ff3
Maxmud <maxmudbobomurodov151@gmail.com>
2025-05-01 20:26:18 +0500

    create student

---
+++ students_menu.py
@@ -0,0 +1,24 @@
+def students_menu():
+    print("""
+    1. Show groups
+    2. Upload homework (id)
+    3. Show my all attendance
+    4. Show my balance
+    5. Payment
+    6. Logout
+    """)
+    choice = input("Enter your choice:  ")
+    if choice == "1":
+        pass
+    elif choice == "2":
+        pass
+    elif choice == "3":
+        pass
+    elif choice == "4":
+        pass
+    elif choice == "5":
+        pass
+    elif choice == "6":
+        pass
+    else:
+        print("Invalid choice")
