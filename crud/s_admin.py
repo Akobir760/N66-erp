8d55e6bcbb5010ea84798f9e2dfe4a0619b997cc
Akobir <akobirortiqov40@gmail.com>
2025-05-01 20:32:50 +0500

    change filemanager

---
+++ crud/s_admin.py
@@ -0,0 +1,9 @@
+# Supera admin menu:
+#     1. Show all admins
+#     2. Create admin
+#     3. Delete admin
+#     4. Show statistics
+#     5. Logout
+
+
+from filemanager import read, write, append
